d8f3d02a336584cd19d2276603ce8bf8ef96a306
Lauren <laurenkaitlyn@gmail.com>
2023-11-23 16:43:22 -0500

    add solutions page

---
+++ catalyst-commons/pages/office-solutions.jsx
@@ -0,0 +1,12 @@
+import Link from "next/Link";
+
+
+export default function OfficeSolutions () {
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <br></br>
+      Office Solutions
+    </div>
+  )
+}
