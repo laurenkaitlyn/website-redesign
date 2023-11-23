7f7c921f442e236a3a39c7faca7f19dceb3f3e58
Lauren <laurenkaitlyn@gmail.com>
2023-11-23 16:42:27 -0500

    add events page

---
+++ catalyst-commons/pages/community-events.jsx
@@ -0,0 +1,11 @@
+import Link from "next/Link";
+
+export default function CommunityEvents () {
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <br></br>
+      Community Events
+    </div>
+  )
+}
