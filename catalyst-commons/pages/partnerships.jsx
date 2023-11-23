6374e3e8a97015d1ac4fe01fa45aec692cffe457
Lauren <laurenkaitlyn@gmail.com>
2023-11-23 16:44:34 -0500

    add partnership page

---
+++ catalyst-commons/pages/partnerships.jsx
@@ -0,0 +1,12 @@
+import Link from "next/Link";
+
+
+export default function Partnerships () {
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <br></br>
+      Partnerships
+    </div>
+  )
+}
